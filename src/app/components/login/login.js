c01931dc53f308465bc589de7f624e56d782fe57
Tom Fischer <tgfischer6@gmail.com>
2016-12-23 14:02:26 -0500

    Working on login route

--- src/app/components/login/login.js
+++ src/app/components/login/login.js
@@ -1,16 +1,13 @@
 import React, {Component} from 'react';
 import {NavBar} from '../navbar';
+import {Button, Container, Form, Grid, Header, Message, Segment} from 'semantic-ui-react';
 
 const styles = {
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100%'
+    height: 'calc(100% - 2.85714286em)'
   },
-  main: {
-    flex: 1,
-    display: 'flex',
-    flexDirection: 'column'
+  grid: {
+    height: '100%'
   }
 };
 
@@ -19,6 +16,30 @@ export class Login extends Component {
     return (
       <div style={styles.container}>
         <NavBar/>
+        <Container style={styles.container} text>
+          <Grid verticalAlign="middle" style={styles.grid}>
+            <Grid.Column>
+              <Header as="h1">Log in</Header>
+
+              <Segment basic>
+                <Form size="huge">
+                  <Form.Input name="email" label="Email" type="text"/>
+                  <Form.Input name="password" label="Password" type="password"/>
+                  <Button size="huge" type="submit" primary>Log In</Button>
+                </Form>
+              </Segment>
+
+              <Message info>
+                <Message.Header>
+                  New to Shareff?
+                </Message.Header>
+                <p>
+                  Sign up for a Shareff account today!
+                </p>
+              </Message>
+            </Grid.Column>
+          </Grid>
+        </Container>
       </div>
     );
   }
