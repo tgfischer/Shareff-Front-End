2641b634811f9f03185943121f4d996f33e0dd35
David <dnisenbaum1@gmail.com>
2017-01-05 14:02:22 -0500

    added basic stripe pay button at /payment endpoint

---
+++ src/app/components/payment/payment.js
@@ -0,0 +1,39 @@
+import React, {Component} from 'react';
+import {NavBar} from '../navbar';
+import StripeCheckout from 'react-stripe-checkout';
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100%'
+  },
+  main: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column'
+  }
+};
+
+export class Payment extends Component {
+  onToken(token) {
+    fetch('/save-stripe-token', {
+      method: 'POST',
+      body: JSON.stringify(token)
+    }).then(token => {
+      console.log(`We are in business, ${token.email}`);
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <NavBar/>
+        <StripeCheckout
+          token={this.onToken}
+          stripeKey="pk_test_aVtElBsIJz1fgHUdj1AkvGkJ"
+          />
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import {Router, Route, browserHistory} from 'react-router';
 import {Home} from './app/components/home/home';
 import {Login} from './app/components/login/login';
 import {SignUp} from './app/components/signup/signup';
+import {Payment} from './app/components/payment/payment';
 
 import './index.scss';
 
@@ -15,6 +16,7 @@ ReactDOM.render(
     <Route path="/" component={Home}/>
     <Route path="/login" component={Login}/>
     <Route path="/signup" component={SignUp}/>
+    <Route path="/payment" component={Payment}/>
   </Router>,
   document.getElementById('root')
 );
