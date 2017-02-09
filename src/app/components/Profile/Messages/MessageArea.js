a51d86e6980fa32985c7797bdcb8b0ef60976b79
Tom Fischer <tgfischer6@gmail.com>
2017-02-09 18:33:13 -0500

    Messaging works

--- src/app/components/Profile/Messages/MessageArea.js
+++ src/app/components/Profile/Messages/MessageArea.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {withRouter} from 'react-router';
 import {
-  Divider, Form, Grid, Header, Image, Segment
+  Button, Divider, Form, Grid, Header, Image, Modal, Segment
 } from 'semantic-ui-react';
 import {intlShape, injectIntl, FormattedMessage} from 'react-intl';
 import {BASE_URL} from '../../../constants/constants';
@@ -20,11 +20,18 @@ class MessageArea extends Component {
     contentColumnPadding: 0,
     contentSegmentBorderWidth: 0,
     conversationHeaderHeight: 0,
-    messageInputHeight: 0
+    messageInputHeight: 0,
+    dividerBorderWidth: 0,
+    messages: [],
+    err: null
   }
   constructor(props) {
     super(props);
     this.handleSendMessage = this.handleSendMessage.bind(this);
+    this.handleCloseModal = this.handleCloseModal.bind(this);
+    this.state = {
+      messages: props.messages
+    };
   }
   componentDidMount() {
     // Get the height of all of the elements that are above and below the message
@@ -39,6 +46,7 @@ class MessageArea extends Component {
     const contentSegmentBorderWidth = Number.parseInt($('.content-segment').css('border-width').replace('px', ''), 10) * 2;
     const conversationHeaderHeight = $('.conversation-header').outerHeight();
     const messageInputHeight = $('.message-input').outerHeight();
+    const dividerBorderWidth = Number.parseInt($('.separator').css('border-width').replace('px', ''), 10) * 2;
 
     this.setState({navBarHeight});
     this.setState({pageHeaderHeight});
@@ -47,10 +55,29 @@ class MessageArea extends Component {
     this.setState({contentSegmentBorderWidth});
     this.setState({conversationHeaderHeight});
     this.setState({messageInputHeight});
+    this.setState({dividerBorderWidth});
 
-    socket.emit('subscribe', this.props.conversationId);
-    socket.on('receive:message', message => {
-      console.log(message);
+    const {conversationId, user} = this.props;
+    const {userId} = user;
+    const token = localStorage.getItem('token');
+
+    // Connect to the chatroom
+    socket.emit('subscribe', {
+      conversationId,
+      userId,
+      token
+    });
+
+    // Receive the messages from the server
+    socket.on('receive:messages', messages => {
+      // Update the messages in the chatbox
+      this.setState({messages});
+    });
+
+    // Receive the messages from the server
+    socket.on('error', err => {
+      // Update the messages in the chatbox
+      this.setState({err});
     });
   }
   componentDidUpdate() {
@@ -60,21 +87,30 @@ class MessageArea extends Component {
   handleSendMessage(e, {formData}) {
     e.preventDefault();
 
-    const {conversationId} = this.props;
-    const {message} = formData;
+    const {conversationId, user} = this.props;
+    const {userId} = user;
+    const {content} = formData;
+    const token = localStorage.getItem('token');
 
+    // Send the message to the server
     socket.emit('send:message', {
-      room: conversationId,
-      message
+      senderId: userId,
+      conversationId,
+      content,
+      token
     });
+
+    // Clear the input
+    e.target[0].value = "";
   }
+  handleCloseModal = () => this.setState({err: null})
   render() {
     const {
       navBarHeight, pageHeaderHeight, verticalSegmentPadding,
       contentColumnPadding, contentSegmentBorderWidth, conversationHeaderHeight,
-      messageInputHeight
+      messageInputHeight, dividerBorderWidth, messages, err
     } = this.state;
-    const {intl, messages, recipient, item, rentRequest, user} = this.props;
+    const {intl, recipient, item, rentRequest, user} = this.props;
     const {firstName, lastName, photoUrl} = recipient;
     const {startDate, endDate} = rentRequest;
     const {formatMessage} = intl;
@@ -86,7 +122,7 @@ class MessageArea extends Component {
         maxHeight: `calc(100vh - ${navBarHeight}px - ${pageHeaderHeight}px - \
           ${verticalSegmentPadding}px - ${contentColumnPadding}px - \
           ${contentSegmentBorderWidth}px - ${conversationHeaderHeight}px - \
-          ${messageInputHeight}px)`,
+          ${messageInputHeight}px - ${dividerBorderWidth}px)`,
         overflow: 'auto'
       },
       messageGrid: {
@@ -99,60 +135,83 @@ class MessageArea extends Component {
     };
 
     return (
-      <Grid>
-        <Grid.Row className="conversation-header" verticalAlign="middle">
-          <Grid.Column>
-            <Header as="h1">
-              <Image src={BASE_URL + photoUrl} shape="rounded"/>
-              <Header.Content>
-                <FormattedMessage id="messages.title" values={{firstName, lastName}}/>
-                <Header.Subheader>
-                  <FormattedMessage id="messages.requestedItem" values={{item: item.title}}/>
-                </Header.Subheader>
-                <Header.Subheader>
-                  {moment(startDate).format('MMM Do YYYY, h:mm a')} &#8594; {moment(endDate).format('MMM Do YYYY, h:mm a')}
-                </Header.Subheader>
-              </Header.Content>
-            </Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Divider style={styles.divider}/>
-        <Grid.Row style={styles.messageArea} className="message-area">
-          <Grid style={styles.messageGrid}>
-            {messages.map(({senderId, timeSent, content}, i) => {
-              // Check if this message was sent by the logged in user, or the
-              // recipient
-              const floated = user.userId === senderId ? 'right' : undefined;
-              const colour = user.userId === senderId ? 'blue' : undefined;
+      <div>
+        <Grid>
+          <Grid.Row className="conversation-header" verticalAlign="middle">
+            <Grid.Column>
+              <Header as="h1">
+                <Image src={BASE_URL + photoUrl} shape="rounded"/>
+                <Header.Content>
+                  <FormattedMessage id="messages.title" values={{firstName, lastName}}/>
+                  <Header.Subheader>
+                    <FormattedMessage id="messages.requestedItem" values={{item: item.title}}/>
+                  </Header.Subheader>
+                  <Header.Subheader>
+                    {moment(startDate).format('MMM Do YYYY, h:mm a')} &#8594; {moment(endDate).format('MMM Do YYYY, h:mm a')}
+                  </Header.Subheader>
+                </Header.Content>
+              </Header>
+            </Grid.Column>
+          </Grid.Row>
+          <Divider style={styles.divider} className="separator"/>
+          <Grid.Row style={styles.messageArea} className="message-area">
+            <Grid style={styles.messageGrid}>
+              {messages.map(({senderId, timeSent, content}, i) => {
+                // Check if this message was sent by the logged in user, or the
+                // recipient
+                const floated = user.userId === senderId ? 'right' : undefined;
+                const colour = user.userId === senderId ? 'blue' : undefined;
 
-              // Render the message in the message area
-              return (
-                <Grid.Row key={i}>
-                  <Grid.Column floated={floated} width={10}>
-                    <Segment size="large" floated={floated} color={colour} compact>
-                      {content}
-                    </Segment>
-                  </Grid.Column>
-                </Grid.Row>
-              );
-            })}
-          </Grid>
-        </Grid.Row>
-        <Grid.Row className="message-input" verticalAlign="bottom">
-          <Grid.Column>
-            <Form onSubmit={this.handleSendMessage} size="large">
-              <Form.Input
-                label={formatMessage({id: 'messages.inputLabel'})}
-                action={{color: 'blue', labelPosition: 'right', icon: 'send', content: formatMessage({id: 'messages.sendButton'}), size: 'large'}}
-                name="message"
-                placeholder={formatMessage({id: 'messages.inputPlaceholder'})}
-                type="text"
-                required
-                />
-            </Form>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
+                // Render the message in the message area
+                return (
+                  <Grid.Row key={i}>
+                    <Grid.Column floated={floated} width={10}>
+                      <Segment size="large" floated={floated} color={colour} compact>
+                        {content}
+                      </Segment>
+                    </Grid.Column>
+                  </Grid.Row>
+                );
+              })}
+            </Grid>
+          </Grid.Row>
+          <Grid.Row className="message-input" verticalAlign="bottom">
+            <Grid.Column>
+              <Form onSubmit={this.handleSendMessage} size="large">
+                <Form.Input
+                  label={formatMessage({id: 'messages.inputLabel'})}
+                  action={{color: 'blue', labelPosition: 'right', icon: 'send', content: formatMessage({id: 'messages.sendButton'}), size: 'large'}}
+                  name="content"
+                  placeholder={formatMessage({id: 'messages.inputPlaceholder'})}
+                  autoComplete="off"
+                  type="text"
+                  required
+                  />
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+        <Modal size="small" dimmer="blurring" open={Boolean(err)} onClose={this.handleCloseModal}>
+          <Modal.Header>
+            <Header as="h1">
+              <FormattedMessage id="modal.error"/>
+            </Header>
+          </Modal.Header>
+          <Modal.Content>
+            <Header as="h3">
+              <FormattedMessage id="error.general"/>
+            </Header>
+          </Modal.Content>
+          <Modal.Actions>
+            <Button
+              content={formatMessage({id: 'modal.okay'})}
+              onClick={this.handleCloseModal}
+              size="huge"
+              primary
+              />
+          </Modal.Actions>
+        </Modal>
+      </div>
     );
   }
 }
